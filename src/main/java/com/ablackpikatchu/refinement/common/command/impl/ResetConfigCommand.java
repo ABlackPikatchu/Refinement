6ce4f1e00fb6ed62620d5d5d8bab3c5bedf97a87
matyrobbrt <robertmateimoraru@gmail.com>
2021-10-07 14:24:02 +0300

    yes

---
+++ src/main/java/com/ablackpikatchu/refinement/common/command/impl/ResetConfigCommand.java
@@ -0,0 +1,54 @@
+package com.ablackpikatchu.refinement.common.command.impl;
+
+import static net.minecraft.command.Commands.literal;
+
+import com.ablackpikatchu.refinement.common.command.BaseCommand;
+import com.ablackpikatchu.refinement.core.config.ModJsonConfigs;
+import com.ablackpikatchu.refinement.core.config.json.JsonConfig;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+
+import net.minecraft.command.CommandSource;
+import net.minecraft.util.text.StringTextComponent;
+
+public class ResetConfigCommand extends BaseCommand {
+
+	public ResetConfigCommand(int permissionLevel, boolean enabled) {
+		super(permissionLevel, enabled);
+	}
+
+	@Override
+	public void build(LiteralArgumentBuilder<CommandSource> builder) {
+		for (Type type : Type.values()) {
+			builder.then(literal(type.name().toUpperCase()).executes(context -> this.resetConfigs(context, type)));
+		}
+	}
+
+	private int resetConfigs(CommandContext<CommandSource> context, Type type) throws CommandSyntaxException {
+		type.resetConfig(context);
+		return 0;
+	}
+
+	@Override
+	public String getName() {
+		return "reset_config";
+	}
+
+	private enum Type {
+		RESOURCE_STATUE(ModJsonConfigs.RESOURCE_STATUE), LOOT_BOXES(ModJsonConfigs.LOOT_BOXES);
+
+		private final JsonConfig config;
+
+		private Type(JsonConfig config) {
+			this.config = config;
+		}
+
+		private void resetConfig(CommandContext<CommandSource> context) {
+			this.config.generateConfig();
+			context.getSource().sendSuccess(
+					new StringTextComponent("The " + this.config.getName() + " config has been reset!"), true);
+		}
+	}
+
+}
--- src/main/java/com/ablackpikatchu/refinement/core/init/CommandInit.java
+++ src/main/java/com/ablackpikatchu/refinement/core/init/CommandInit.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import com.ablackpikatchu.refinement.Refinement;
 import com.ablackpikatchu.refinement.common.command.BaseCommand;
 import com.ablackpikatchu.refinement.common.command.impl.ReloadCommand;
+import com.ablackpikatchu.refinement.common.command.impl.ResetConfigCommand;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 
@@ -22,6 +23,7 @@ public class CommandInit {
 		CommandDispatcher<CommandSource> dispatcher = event.getDispatcher();
 		
 		commands.add(new ReloadCommand(2, true));
+		commands.add(new ResetConfigCommand(2, true));
 		
 		commands.forEach(command -> {
 			if (command.isEnabled()) {
